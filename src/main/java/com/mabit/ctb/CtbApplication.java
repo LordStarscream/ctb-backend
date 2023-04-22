be57d445d979f533c7132c3daea57436b12b24d6
Mario Bittner <MarioBittner@gmx.de>
2023-04-22 17:57:54 +0200

    init spring-boot

---
+++ src/main/java/com/mabit/ctb/CtbApplication.java
@@ -0,0 +1,13 @@
+package com.mabit.ctb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtbApplication.class, args);
+	}
+
+}
