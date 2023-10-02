12f368cc4c946df1067c75609581109868d698c3
Mario Bittner <MarioBittner@gmx.de>
2023-10-02 15:47:10 +0200

    first working import with frontend. getting fileimporter selection in service adding CorsConfiguration to work with different port of the frontend

---
+++ src/main/java/com/mabit/ctb/configuration/CorsConfiguration.java
@@ -0,0 +1,21 @@
+
+package com.mabit.ctb.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Erlauben Sie Anfragen von Ihrem Angular-Server
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
--- src/main/java/com/mabit/ctb/controller/FileController.java
+++ src/main/java/com/mabit/ctb/controller/FileController.java
@@ -3,6 +3,7 @@ package com.mabit.ctb.controller;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.MediaType;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.mabit.ctb.file.FileImport;
 import com.mabit.ctb.service.FileService;
 
 @RestController
@@ -18,6 +22,9 @@ public class FileController {
 
     @Autowired
     private FileService fileService;
+
+    @Autowired // inject all import implementations
+    private List<FileImport> importServices;
 
     @PostMapping(value = "/file/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadFile(
@@ -29,6 +36,13 @@ public class FileController {
         }catch(IOException ex){
             return ResponseEntity.status((HttpStatus.INTERNAL_SERVER_ERROR)).body("Fehler beim Speichern der Datei: " + ex.getMessage());
         }
+    }
+
+    @GetMapping("/file/importTypes")
+    public List<String> getImportTypes(){
+        return importServices.stream()
+            .map(FileImport::getName)
+            .collect(Collectors.toList());
     }
 
 }
